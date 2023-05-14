56e70b78153f6589f7a76eaac186bb4af5b7a0a1
wzp <wzpscott@gmail.com>
2023-05-14 13:50:56 +0800

    fix wrong method name in run.py

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import os
 for scene in ["chair", "drums", "ficus", "hotdog", "lego", "materials", "mic"]:
     os.system(f"ns-train egnerf --method-name EventNeRF --experiment-name {scene} --data {scene} --pipeline.model.use-original-event-nerf True")
     os.system(f"ns-train egnerf --method-name EgNeRF --experiment-name {scene} --data {scene}")
-    os.system(f"ns-train egnerf --method-name EgNeRF --experiment-name {scene} --data {scene} --pipeline.model.event-loss-mult 0")
+    os.system(f"ns-train egnerf --method-name NeRF --experiment-name {scene} --data {scene} --pipeline.model.event-loss-mult 0")
